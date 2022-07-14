61478f8b953f89369d05b86ef7880a1b8b9fe830
Evandro <evandro.ribeiro@sellbie.com.br>
2022-07-14 19:21:51 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dsmeta.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.devsuperior.dsmeta.entities.Sales;
+
+public interface SaleRepository extends JpaRepository<Sales, Long> {
+	
+}
